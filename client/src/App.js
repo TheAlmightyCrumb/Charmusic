8306b5b8f13ea2cb3f15f211d7e323812a05b0eb
TheAlmightyCrumb <67858097+TheAlmightyCrumb@users.noreply.github.com>
2020-09-13 23:41:01 +0300

    fonts and style template

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <div className="header">
         <img src={logo} id="logo" alt="logo"/>
+        <a className="link" href="https://github.com/TheAlmightyCrumb" target="_blank" rel="noopener noreferrer">@TheAlmightyCrumb on GitHub</a>
       </div>
       <div className="container">
         <div className="left"></div>
