0da17b1a6295538a5e1ded256570a62f7c4ebf1d
TheAlmightyCrumb <67858097+TheAlmightyCrumb@users.noreply.github.com>
2020-09-21 15:03:34 +0300

    songPage style

--- client/src/components/SongPage.js
+++ client/src/components/SongPage.js
@@ -2,16 +2,38 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import SideSongs from './SideSongs';
 import { Link } from 'react-router-dom';
+import './SongPage.css';
 
 export default function SongPage({ match, location }) {
 
     const [songInfo, setSongInfo] = useState([]);
+    const [artistImg, setArtistImg] = useState('');
+    const [albumImg, setAlbumImg] = useState('');
 
     const showSongInfo = async () => {
         const { data } = await axios.get(`/songs/${match.params.id}`);
-        console.log(data);
+        // console.log(data);
         setSongInfo(...data);
+        const album = await axios.get(`/albums/${data[0].Album_id}`);
+        setAlbumImg(
+                <Link to={`/albums/${data[0].Album_id}`} className='songPageLink'>
+                    <div className='song-info-image-container'>
+                        <img src={album.data[0].Cover_img} alt={album.data[0].Album_Name} className='song-info-image' />
+                    </div>
+                    <div className='song-info-title'> {album.data[0].Album_Name} </div>
+                </Link>
+        );
+        const artist = await axios.get(`/artists/${data[0].Artist_id}`);
+        setArtistImg(
+                <Link to={`/artists/${data[0].Artist_id}`} className='songPageLink'>
+                    <div className='song-info-image-container'>
+                        <img src={artist.data[0].Cover_img} alt={artist.data[0].Artist_Name} className='song-info-image' />
+                    </div>
+                    <div className='song-info-title'> {artist.data[0].Artist_Name} </div>
+                </Link>
+        );
     }
+
 
     useEffect(() => {
         showSongInfo()
@@ -26,12 +48,21 @@ export default function SongPage({ match, location }) {
 
     return (
         <div>
-            Title: {songInfo.Title} <br />
-            Artist: <Link to={`/artists/${songInfo.Artist_id}`}> {songInfo.Artist_Name} </Link><br />
-            Album: <Link to={`/albums/${songInfo.Album_id}`}> {songInfo.Album_Name} </Link><br />
-            <iframe onLoad={() => fullScreen()} className='media' src={songInfo.Youtube} frameBorder="0" allow="autoplay"></iframe><br />
-            Length: {songInfo.Length}<br />
-            Lyrics: {songInfo.Lyrics}
+            {/* Title: {songInfo.Title} <br /> */}
+            {/* Artist: <Link to={`/artists/${songInfo.Artist_id}`}> {songInfo.Artist_Name} </Link><br /> */}
+            {/* Album: <Link to={`/albums/${songInfo.Album_id}`}> {songInfo.Album_Name} </Link><br /> */}
+            {/* Album: <Link to={`/albums/${songInfo.Album_id}`}> <img src={albumImg} /> </Link><br /> */}
+            {/* <iframe onLoad={() => fullScreen()} className='media' src={songInfo.Youtube} frameBorder="0" allow="autoplay"></iframe><br /> */}
+            {/* Length: {songInfo.Length}<br /> */}
+            {/* Lyrics: {songInfo.Lyrics} */}
+            <h1 id='songTitle'>{songInfo.Title}</h1>
+            <section id='info-section'>
+                <h3 id='info'>Info</h3>
+                <div id='info-images-container'>
+                    <div>{artistImg}</div>
+                    <div>{albumImg}</div>
+                </div>
+            </section>
             <SideSongs query={location.search} idParam={match.params.id} />
         </div>
     )
